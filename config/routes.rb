2abb8aeb0acab30fa2c6738fdb978fc9da4418cd
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-05-08 22:20:34 +0200

    Add users resource as front-end structure for stock lookup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'welcome#index'
+  devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
 end
