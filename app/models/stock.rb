3865dfe5a190cd1ae10bdea23052fb92834c78ad
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-05-08 19:03:05 +0200

    Add stock model and iex api key (encrypt)

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+            secret_token: Rails.application.credentials.iex_client[:secret_api_key],
+            endpoint: 'https://iexcloud.io/docs/core'
+        )
+        client.price('ticker_symbol')
+    end
 end
