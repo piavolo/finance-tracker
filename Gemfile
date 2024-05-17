cf4e5e050745df35d66ce04b6370f49ba81f4415
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-05-17 14:33:25 +0200

    Display stock price in browser, stock controller and model (Alpha Vantage gem)

--- Gemfile
+++ Gemfile
@@ -59,6 +59,10 @@ gem 'iex-ruby-client'
 # Font Awesome icons gem
 gem "font-awesome-rails"
 
+# AlphaVantage API gems
+gem 'alphavantage'
+gem 'httparty'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,32 @@
+# class Stock < ApplicationRecord
+#     def self.new_lookup(ticker_symbol)
+#         client = IEX::Api::Client.new(
+#             publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+#             secret_token: Rails.application.credentials.iex_client[:secret_api_key],
+#             endpoint: "https://api.iex.cloud/v1/data/core/iex_deep/#{ticker_symbol}?token=#{Rails.application.credentials.iex_client[:sandbox_api_key]}"
+#         )
+#         client.price(ticker_symbol)
+#     end
+# end
+
+require 'httparty'
+ 
 class Stock < ApplicationRecord
+ 
+    def self.company_lookup(ticker_symbol)
+        response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:key]}")
+        response.code == 200 ? { symbol: ticker_symbol, name: response.parsed_response['Name'] }.to_json : nil
+    end
+ 
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(
-            publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-            secret_token: Rails.application.credentials.iex_client[:secret_api_key],
-            endpoint: 'https://iexcloud.io/docs/core'
-        )
-        client.price('ticker_symbol')
+        response = HTTParty.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:key]}")
+        if response.code == 200 && response.parsed_response['Global Quote']
+            last_price = response.parsed_response['Global Quote']['05. price']
+            name = company_lookup(ticker_symbol)
+            { symbol: ticker_symbol, name: name, last_price: last_price }.to_json
+            # { symbol: ticker_symbol, last_price: last_price, company_info: company_lookup(ticker_symbol) }.to_json
+        else
+            nil
+        end
     end
 end
