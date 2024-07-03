f43ac789e65ee000251ddb251202b387d8866294
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-07-03 02:17:57 +0200

    Setup UserStock resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -12,6 +12,10 @@
 require 'httparty'
 
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
 
     def self.company_info(ticker_symbol)
         response = HTTParty.get("https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{ticker_symbol}&apikey=#{Rails.application.credentials.alphavantage[:key]}")
