26fff74bdc24f8c3c8df1df9722fcda5a30e66f8
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-07-10 01:02:13 +0200

    Track stocks from front-end (browser)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
