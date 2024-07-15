1e9dab8248d733c5c871b361ad70ff220988e71c
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-07-15 12:39:19 +0200

    Create search friend form

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'search_friend', to: 'users#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
 end
